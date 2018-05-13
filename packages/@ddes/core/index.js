5d6deed3cc5cddcb589d34c15ae2c8507756beef
Trym Skaar <trym.skaar@gmail.com>
2018-05-13 17:41:47 +0200

    chore: make packages requireable with ts-node

--- packages/@ddes/core/index.js
+++ packages/@ddes/core/index.js
@@ -6,9 +6,11 @@ Symbol.asyncIterator =
 if (semver.satisfies(process.versions.node, '<8.10')) {
   throw new Error('NodeJS >=8.10 or newer is required')
 } else if (semver.satisfies(process.versions.node, '>=8.10.0 <10')) {
-  // tslint:disable-next-line:no-var-requires
   module.exports = require('./dist/node-8.10')
 } else if (semver.satisfies(process.versions.node, '>=10')) {
-  // tslint:disable-next-line:no-var-requires
-  module.exports = require('./dist/node-10')
+  try {
+    module.exports = require('./dist/node-10')
+  } catch (error) {
+    module.exports = require('./lib')
+  }
 }
