cc9dd1f515083eb477ddb8999cff2cc302e2b081
Trym Skaar <trym.skaar@gmail.com>
2018-05-14 03:07:15 +0200

    feat: CLI
    @ddes/cli package exposes the "ddes" exexcutable, which loads commands
    from other @ddes packages

---
+++ packages/@ddes/cli/lib/cli.ts
@@ -0,0 +1,35 @@
+/**
+ * @module @ddes/cli
+ */
+
+import {pickBy} from 'lodash'
+import * as parseArgs from 'minimist'
+import displayUsage from './displayUsage'
+import loadCommands from './loadCommands'
+
+export default async function cli() {
+  const commandName = process.argv[2]
+  const commands = await loadCommands()
+  const matchingCommands = pickBy(commands, (v, k) => k.startsWith(commandName))
+  let command = commands[commandName]
+
+  if (!command && Object.keys(matchingCommands).length === 1) {
+    command = commands[Object.keys(matchingCommands)[0]]
+  }
+
+  if (!command) {
+    displayUsage(
+      Object.keys(matchingCommands).length ? matchingCommands : commands
+    )
+  } else {
+    try {
+      const params = command.params(parseArgs(process.argv.slice(3)))
+      await command.handler(params)
+    } catch (error) {
+      console.error(`Error: ${error.message}`)
+      console.log('')
+      console.log(`Usage:`)
+      console.log(`ddes ${commandName} ${command.usage()}\n`)
+    }
+  }
+}
